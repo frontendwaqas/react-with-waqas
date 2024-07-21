4ac208bf888ff6223edcd821e38f6042b3bec3b8
Muhammad Waqas <frontend.waqas@gmail.com>
2024-07-21 04:23:05 -0700

    Add Counter

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,7 +1,82 @@
+import { useState } from "react";
+
 function App() {
+  const [value, setValue] = useState(1);
+  function addCounter() {
+    if (value >= 0) {
+      setValue((prev) => prev + 1);
+    } else {
+      alert("Somthing Want Wrong!");
+      setValue(1);
+    }
+  }
+
+  function removeCounter() {
+    if (value >= 0) {
+      setValue((prev) => prev - 1);
+    } else {
+      alert("Somthing Want Wrong!");
+      setValue(1);
+    }
+  }
+
   return (
     <>
-      <h1>Hello</h1>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          margin: "auto",
+          gap: "3px",
+        }}
+      >
+        <h1>Hello Rai!</h1>
+        <h1>Counter Value : {value}</h1>
+        <div className="">
+          <button
+            onClick={addCounter}
+            style={{
+              backgroundColor: "#4CAF50" /* Green */,
+              border: "none",
+              color: "white",
+              padding: "15px 32px",
+              textAlign: "center",
+              textDecoration: "none",
+              display: "inline-block",
+              fontSize: "16px",
+              margin: "4px 2px",
+              cursor: "pointer",
+              borderRadius: "12px",
+              transitionDuration: "0.4s",
+            }}
+          >
+            ADD
+          </button>
+          <button
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              border: "2px solid #4CAF50",
+              padding: "15px 32px",
+              textAlign: "center",
+              textDecoration: "none",
+              display: "inline-block",
+              fontSize: "16px",
+              margin: "4px 2px",
+              cursor: "pointer",
+              borderRadius: "12px",
+              transitionDuration: "0.4s",
+            }}
+            onClick={removeCounter}
+          >
+            Remove
+          </button>
+        </div>
+      </div>
     </>
   );
 }
