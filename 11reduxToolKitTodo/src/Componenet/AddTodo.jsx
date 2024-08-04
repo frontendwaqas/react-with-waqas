cdf4013137c4f8962e0612474780bd5247a7bfd1
Muhammad Waqas <frontend.waqas@gmail.com>
2024-08-04 00:54:24 -0700

    Redux ToolKit

---
+++ 11reduxToolKitTodo/src/Componenet/AddTodo.jsx
@@ -0,0 +1,5 @@
+function AddTodo() {
+  return <div></div>;
+}
+
+export default AddTodo;
---
+++ 11reduxToolKitTodo/src/Componenet/Todos.jsx
@@ -0,0 +1,10 @@
+
+function Todos() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todos
